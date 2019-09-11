77ff9b1fd4c838e966e86a11a055e00eb6983256
Siarhei <spartateambuilder@mail.ru>
2019-09-11 09:16:06 +0300

    Updates

---
+++ precache-manifest.da3cbea5cf2b0f2a80db4272ac582786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc1bed2f3ef53de8ebdc4a3ba9a314e",
+    "url": "/stopWatch/index.html"
+  },
+  {
+    "revision": "bfc73aa021905f1713f2",
+    "url": "/stopWatch/static/css/main.cdcd51c2.chunk.css"
+  },
+  {
+    "revision": "854c11e3c08c37b81915",
+    "url": "/stopWatch/static/js/2.902d41ca.chunk.js"
+  },
+  {
+    "revision": "bfc73aa021905f1713f2",
+    "url": "/stopWatch/static/js/main.2d300675.chunk.js"
+  },
+  {
+    "revision": "6fced1884a6abf3eb05b",
+    "url": "/stopWatch/static/js/runtime~main.7f8a0a82.js"
+  }
+]);
